7965bb9c98121862084c173caac8ceaec853da9e
Ivan Dimitrov <ivan@ivandimitrov.co.uk>
2022-08-07 12:09:17 +0000

    some formatting

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "volume get", 							0,		10},
-	{"", "netstat",								1,		0},
-	{"", "curl wttr.in/Nottingham\?format=\"%C+%t+%p\"",			3600,		0},
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					60,		0},
+	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
+	{"", 		"volume get", 							0,			10},
+	{"", 		"netstat",							1,			0},
+	{"", 		"curl wttr.in/Nottingham\?format=\"%C+%t+%p\"",			3600,			0},
+	{"", 		"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,			0},
+	{"", 		"date '+%b %d (%a) %I:%M%p'",					60,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
